3afe5a14277b02ff83e907cff4af589221a582d2
jefernandezec <fernandezromeroj@gmail.com>
2025-09-30 16:45:34 -0400

    Update 01-processing-data.R
    complete code

--- R/Code/01-processing-data.R
+++ R/Code/01-processing-data.R
@@ -32,22 +32,20 @@ data_tidy_hh <- data_dedup %>%
 
 ### Data cleaning for Household-member (HH-member) level
 data_clean_hh <- data_tidy_hh %>%
-    # Convert submissionday to date
     mutate(submissiondate = as.Date(submissionday, format = "%Y-%m-%d %H:%M:%S")) %>%
-    # Convert duration to numeric (if it is not already)
-    mutate(duration=as.numeric(duration)) %>%
-    # Convert ar_farm_unit to factor (categorical data)
-    mutate(ar_farm_unit=as.factor(ar_farm_unit)) %>%
-    # Replace values in the crop variable based on crop_other using regex for new crops
+    mutate(duration = as.numeric(duration)) %>%
+    mutate(ar_unit = as.factor(ar_farm_unit)) %>%
+    mutate(ar_unit = na_if(ar_unit, "")) %>%
+    mutate(crop_other = str_to_title(crop_other)) %>%
     mutate(crop = case_when(
-        ......
-    )) %>%
-    # Recode negative numeric values (-88) as missing (NA)
-    mutate(across(......)) %>%
-    # Add variable labels
+        str_detect(crop_other, "Coconut") ~ 40,
+        str_detect(crop_other, "Sesame") ~ 41,
+        TRUE ~ crop)) %>%
+    mutate(across(where(is.numeric), ~ replace(., . == -88, NA))) %>%
     set_variable_labels(
-        ......
-    )
+        duration = "Duration of the interview (minutes)",
+        submissiondate = "Submission date",
+        ar_unit = "Farm area unit?")
 
 # Save the household data
 write_dta(data_clean_hh, file.path(data_path, "Intermediate/TZA_CCT_HH.dta"))
@@ -70,14 +68,20 @@ data_tidy_hh_member <- data_dedup %>%
            starts_with("days_impact")) %>%
     pivot_longer(cols = -c(vid, hhid, enid),
                  names_to = c(".value", "member"),
-                 names_pattern = "(.*) (\\d+)")
+                 names_pattern = "(.*)_(\\d+)")
 
 ### Data cleaning for HH-member level
-data_clean_mem <- data_tidy_mem %>%
-    # Drop rows where gender is missing (NA)
-    ...... %>%
-    # Variable labels
-    ......
+data_clean_mem <- data_tidy_hh_member %>%
+    filter(!is.na(gender)) %>%
+    set_variable_labels(
+        member = "HH member ID",
+        age = "Age",
+        clinic_visit = "In the past 12 months, how many times has the member attended the clinic?",
+        days_sick = "No. of days in the last 4 weeks the member suffered from the health problem?",
+        treat_cost = "How much did the treatment cost?",
+        days_impact = "No. of days member was unable to perform daily activities due to illness?"
+    )
+
 
 # Save the tidy household-member data
 write_dta(data_clean_mem, file.path(data_path, "Intermediate/TZA_CCT_HH_mem.dta"))
@@ -89,9 +93,9 @@ secondary_data <- read.csv(file.path(data_path, "Raw/TZA_amenity.csv"))
 
 # Tidying data
 secondary_data <- secondary_data %>%
-    pivot_wider(names_from = ......,
-                values_from = ......,
-                names_prefix = ......)
+    pivot_wider(names_from = amenity,
+                values_from = n,
+                names_prefix = "n_")
 
 # Save the final tidy secondary data
 write_dta(secondary_data, file.path(data_path, "Intermediate/TZA_amenity_tidy.dta"))
