72850edf949e0616c4264d625fce5cb1d20d6a10
jefernandezec <fernandezromeroj@gmail.com>
2025-09-30 16:29:48 -0400

    Update 01-processing-data.R
    updated 01 processing data

--- R/Code/01-processing-data.R
+++ R/Code/01-processing-data.R
@@ -2,36 +2,42 @@
 # 01. Data processing
 
 ### Libraries
-# library(haven)
-# library(dplyr)
-# library(tidyr)
-# library(stringr)
-# library(labelled)
+library(haven)
+library(dplyr)
+library(tidyr)
+library(stringr)
+library(labelled)
 
 ### Loading data ----
 
 # Load the dataset
-data_path <- "ADD-YOUR-PATH"
+data_path <- "C:/Users/wb553773/OneDrive - WBG/Training/Credible Analytics/DataWork/Data/"
 data      <- read_dta(file.path(data_path, "Raw/TZA_CCT_baseline.dta"))
+
+nrow(data)
+
+data %>%
+    select(hhid) %>%
+    n_distinct()
 
 ### Remove duplicates based on hhid
 data_dedup <- data %>%
-    ......
+    distinct(hhid, .keep_all = TRUE)
 
 ### Household (HH) level data ----
 
 #### Tidying data for HH level
 data_tidy_hh <- data_dedup %>%
-    ......
+    select(vid,hhid,enid,floor:n_elder,food_cons:last_col())
 
 ### Data cleaning for Household-member (HH-member) level
 data_clean_hh <- data_tidy_hh %>%
     # Convert submissionday to date
-    mutate(...... = as.Date(......, format = "%Y-%m-%d %H:%M:%S")) %>%
+    mutate(submissiondate = as.Date(submissionday, format = "%Y-%m-%d %H:%M:%S")) %>%
     # Convert duration to numeric (if it is not already)
-    mutate(......) %>%
+    mutate(duration=as.numeric(duration)) %>%
     # Convert ar_farm_unit to factor (categorical data)
-    mutate(......) %>%
+    mutate(ar_farm_unit=as.factor(ar_farm_unit)) %>%
     # Replace values in the crop variable based on crop_other using regex for new crops
     mutate(crop = case_when(
         ......
@@ -49,12 +55,22 @@ write_dta(data_clean_hh, file.path(data_path, "Intermediate/TZA_CCT_HH.dta"))
 ### Household member (HH-member) level data ----
 
 #### Tidying data for HH-member level
-data_tidy_mem <- data_dedup %>%
-    select(......,
-           starts_with(......)) %>%
-    pivot_longer(cols = -c(vid, hhid, enid),  # Keep IDs static
-                 names_to = ......,
-                 names_pattern = "(.*)_(\\d+)")  # Capture the variable and the suffix
+
+data_tidy_hh_member <- data_dedup %>%
+    select(vid, hhid, enid,
+           starts_with("gender"),
+           starts_with("age"),
+           starts_with("read"),
+           starts_with("clinic_visit"),
+           starts_with("sick"),
+           starts_with("days_sick"),
+           starts_with("treat_fin"),
+           starts_with("treat_cost"),
+           starts_with("ill_impact"),
+           starts_with("days_impact")) %>%
+    pivot_longer(cols = -c(vid, hhid, enid),
+                 names_to = c(".value", "member"),
+                 names_pattern = "(.*) (\\d+)")
 
 ### Data cleaning for HH-member level
 data_clean_mem <- data_tidy_mem %>%
